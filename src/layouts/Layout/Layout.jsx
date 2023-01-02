fae0d36cd8f9abb018f3ceb0a9a5c1622256b2d6
feldrok <matias@moravalero.cl>
2023-01-02 14:29:27 -0300

    normalized front

---
+++ src/layouts/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Footer/Footer"
+import Nav from "../Nav/Nav"
+import { Outlet } from "react-router-dom"
+import React from "react"
+
+function Layout() {
+  // Nav & Footer
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
