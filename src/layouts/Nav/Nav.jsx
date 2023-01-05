c6723544a2586e5ee04e261cdb0138382850137a
feldrok <matias@moravalero.cl>
2023-01-05 18:57:06 -0300

    fix nav styiling

--- src/layouts/Nav/Nav.jsx
+++ src/layouts/Nav/Nav.jsx
@@ -15,7 +15,7 @@ const routes = [
   {
     path: "/mycomics",
     name: "My Comics",
-  }
+  },
 ]
 
 function Nav() {
@@ -67,54 +67,46 @@ function Nav() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              d="M11 16H37"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
-            <path
-              d="M11 27H37"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
-            <path
-              d="M11 39H37"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
+            <path d="M11 16H37" strokeWidth="3" strokeLinecap="round" />
+            <path d="M11 27H37" strokeWidth="3" strokeLinecap="round" />
+            <path d="M11 39H37" strokeWidth="3" strokeLinecap="round" />
           </svg>
         </div>
       </div>
       <div className={`mobile-nav ${navigation ? "show-menu" : ""} `}>
         <div className="profile-menu">
           <div className="profile-container">
-          <img className="profile-picture-mobile-nav" src="./userpic.png" alt="logo" />
-          <div>
-            <p className="profile-name">User name</p>
-            <p className="profile-email">random@email.com</p>
-          </div>
-          </div>
-          
-          <div onClick={toggleNav} className="close-button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            width={15}
-            height={10}
-            viewBox="0 0 24 24"
-            strokeWidth="1.2"
-            stroke="currentColor"
-            className="close-icon"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
+            <img
+              className="profile-picture-mobile-nav"
+              src="./userpic.png"
+              alt="logo"
             />
-          </svg>
+            <div>
+              <p className="profile-name">User name</p>
+              <p className="profile-email">random@email.com</p>
+            </div>
+          </div>
+
+          <div onClick={toggleNav} className="close-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              width={15}
+              height={10}
+              viewBox="0 0 24 24"
+              strokeWidth="1.2"
+              stroke="currentColor"
+              className="close-icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
         </div>
-        </div>
-        
+
         <div className="mobile-nav-items">
           <ul className="mobile-nav-links">
             {routes.map((route, index) => (
