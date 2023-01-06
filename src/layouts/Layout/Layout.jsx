856f4f1b26857acc80247d79ef935574a78df6fd
feldrok <matias@moravalero.cl>
2023-01-06 11:40:59 -0300

    Error message from joi response instead of action

--- src/layouts/Layout/Layout.jsx
+++ src/layouts/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css"
 
 import React, { useEffect } from "react"
 import { Slide, ToastContainer, toast } from "react-toastify"
@@ -11,8 +11,13 @@ import { useSelector } from "react-redux"
 function Layout() {
   const commentsStore = useSelector((state) => state.comments)
 
-  const createdCategoryNotify = () => toast.success(commentsStore.message, { autoClose: 3000, theme: "colored" })
-  const errorCategoryNotify = () => toast.error(commentsStore.message, { autoClose: 2000, theme: "colored" })
+  const createdCategoryNotify = () =>
+    toast.success(commentsStore.message, { autoClose: 3000, theme: "colored" })
+  const errorCategoryNotify = () => {
+    commentsStore.comments.response.map((error) =>
+      toast.error(error.message, { autoClose: 2000, theme: "colored" })
+    )
+  }
 
   useEffect(() => {
     if (commentsStore.comments.success === true) {
