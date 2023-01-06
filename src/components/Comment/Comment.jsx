69076c28ee979aea914e974a01112040d178192f
feldrok <matias@moravalero.cl>
2023-01-06 12:53:54 -0300

    show created comment

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,49 @@
+import "./Comment.css"
+
+import React from "react"
+import { useSelector } from "react-redux"
+
+function Comment() {
+  const commentsStore = useSelector((state) => state.comments)
+
+  const renderComment = () => {
+    if (commentsStore.comments.success === true) {
+      return (
+        <div className="comment-container">
+          <div className="comment-user-data">
+            <img src="./userpic.png" alt="" />
+            <h3>Name Lastname</h3>
+          </div>
+          <div className="comment-text">
+            <p>{commentsStore.comments.response?.text}</p>
+          </div>
+          <div className="comment-footer">
+            <div className="comment-footer-left">
+              <div className="comment-counter">
+                <img src="./repliesCounterIcon.png" alt="" />
+                <span className="comment-counter-text">0</span>
+              </div>
+              <div className="comment-buttons">
+                <div className="comment-reply">
+                  <button className="comment-reply-button">Reply</button>
+                  <img src="./replyIcon.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="comment-footer-right">
+              <p>1 sec ago</p>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return <p>There are no comments</p>
+    }
+  }
+
+  return (
+    renderComment()
+  )
+}
+
+export default Comment
--- src/components/ListComments/ListComments.jsx
+++ src/components/ListComments/ListComments.jsx
@@ -1,16 +1,16 @@
 import './ListComments.css'
 
+import Comment from '../Comment/Comment'
 import NewComment from '../NewComment/NewComment'
 import React from "react"
 
 function ListComments({ type, action }) {
-  
-
   return (
     <>
       <div className={type} >
       <div className="close-modal" onClick={action}></div>
         <div className="comments-container">
+          <Comment />
           <NewComment />
         </div>
       </div>
