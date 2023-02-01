baae190dbb494f186444606dd59c7ad092185eb2
feldrok <matias@moravalero.cl>
2023-02-01 13:39:44 -0300

    carousel problem

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,70 +1,77 @@
-import "./Carousel.css";
+import "./Carousel.css"
 
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
-import Slide from "./Slide";
-import comicActions from "../../store/comics/actions";
+import Slide from "./Slide"
+import comicActions from "../../store/comics/actions"
 
-const { getComics } = comicActions;
+const { getComics } = comicActions
 
 export default function Carousel() {
-  const [slide, setSlide] = useState(0);
-  const dispatch = useDispatch();
+    const [slide, setSlide] = useState(0)
+    const dispatch = useDispatch()
+    const [isLogged, setIsLogged] = useState(false)
 
-  const comicStore = useSelector((store) => store?.comics);
+    const comicStore = useSelector((store) => store?.comics)
 
-  useEffect(() => {
-    dispatch(getComics(5));
-  }, []);
+    useEffect(() => {
+        dispatch(getComics(5))
+        let token = localStorage.getItem("token")
+        if (token || token === undefined) {
+            setIsLogged(true)
+        } else {
+            setIsLogged(false)
+        }
+    }, [])
 
-let sortedComics = comicStore.comics?.response
+    let sortedComics = comicStore.comics?.response
 
+    useEffect(() => {
+        let interval = setInterval(() => {
+            nextSlide()
+        }, 3000)
+        return () => clearInterval(interval)
+    })
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      nextSlide();
-    }, 3000);
-    return () => clearInterval(interval);
-  });
+    const nextSlide = (e) => {
+        setSlide(slide === sortedComics.length - 1 ? 0 : slide + 1)
+    }
+    const siguienteSlide2 = (e) => {
+        e.preventDefault()
+        setSlide(slide === sortedComics.length - 1 ? 0 : slide + 1)
+    }
+    const anteriorSlide = (e) => {
+        e.preventDefault()
+        setSlide(slide === 0 ? sortedComics.length - 1 : slide - 1)
+    }
 
+    const renderCarousel = () => {
+        if (comicStore.comics.length === 0) {
+            return <p>Loading</p>
+        } else {
+            return (
+                <>
+                    <div className="slideImage">
+                        <Slide url={sortedComics[slide]?.photo} />
+                        <div className="text">{sortedComics[slide]?.title}</div>
+                    </div>
+                </>
+            )
+        }
+    }
 
-  const nextSlide = (e) => {
-    setSlide(slide === sortedComics.length - 1 ? 0 : slide + 1);
-  };
-  const siguienteSlide2 = (e) => {
-    e.preventDefault();
-    setSlide(slide === sortedComics.length - 1 ? 0 : slide + 1);
-  };
-  const anteriorSlide = (e) => {
-    e.preventDefault();
-    setSlide(slide === 0 ? sortedComics.length - 1 : slide - 1);
-  };
-
-	const renderCarousel = () => {
-		if (comicStore.comics.length === 0) {
-			return <p>Loading</p>
-		} else {
-			return (<>
-			<div className="slideImage">
-			<Slide url={sortedComics[slide]?.photo} />
-			<div className="text">{sortedComics[slide]?.title}</div>
-			</div>
-			</>)
-		}
-	}
-
-  return (
-    <div className="padre">
-      <div className="carrusel">
-					{renderCarousel()}
-        <a href="." className="prev" onClick={anteriorSlide}>
-          &#10094;
-        </a>
-        <a href="." className="next" onClick={siguienteSlide2}>
-          &#10095;
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="padre">
+            <div className="carrusel">
+                {isLogged ? renderCarousel() : <p>Loading</p>}
+                <a href="." className="prev" onClick={anteriorSlide}>
+                    &#10094;
+                </a>
+                <a href="." className="next" onClick={siguienteSlide2}>
+                    &#10095;
+                </a>
+            </div>
+        </div>
+    )
 }
--- src/router/Home/Home.jsx
+++ src/router/Home/Home.jsx
@@ -7,23 +7,26 @@ import Hero from "../../components/Hero/Hero"
 import React from "react"
 
 function Home() {
-  return (
-    <>
-      <Hero>
+    return (
         <>
-          <h1 className="hero-title">Welcome to Minga! Your favorite comic and manga website!</h1>
-          <p className="hero-text">
-Are you ready to dive deep into the annals of comic and manga history?
-          </p>
-          <Link className="get-started-btn" to={"/"}>
-            Begin Your Journey →
-          </Link>
+            <Hero>
+                <>
+                    <h1 className="hero-title">
+                        Welcome to Minga! Your favorite comic and manga website!
+                    </h1>
+                    <p className="hero-text">
+                        Are you ready to dive deep into the annals of comic and
+                        manga history?
+                    </p>
+                    <Link className="get-started-btn" to={"/"}>
+                        Begin Your Journey →
+                    </Link>
+                </>
+            </Hero>
+            <Carousel />
+            <Outlet />
         </>
-      </Hero>
-      <Carousel/>
-      <Outlet />
-    </>
-  )
+    )
 }
 
 export default Home
